dc8a053bbdd62036663bde6ef1501b56e63b9996
Pierre-Alexandre Klein <pierre-alexandre.klein@student-cs.fr>
2023-01-17 18:49:52 +0100

    correction erreur calcul distance

--- src/merge.py
+++ src/merge.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 
 from geopy.distance import great_circle
+import tqdm
 
 
 def filter_top_nine_citys_only(df_acc_, df_infra_):
@@ -23,7 +24,12 @@ def filter_top_nine_citys_only(df_acc_, df_infra_):
 
 #usage : df_acc, df_infra = filter_top_nine_citys_only(df_acc, df_infra)
 
-
+def distance(row, lat_acc, long_acc):
+    dist = great_circle((row['latitude_dep'], row['longitude_dep']),(lat_acc, long_acc)).meters
+    if dist < 20: 
+        return dist
+    else:
+        return np.nan
 
 def associate_infra_index_to_acc(df_acc, df_infra):
     """ 
@@ -39,22 +45,21 @@ def associate_infra_index_to_acc(df_acc, df_infra):
 
     for code in top_nine_citys:
 
-        print(code)
-
         #Select a sub-dataset (a city) in order to reduce research computations among infrastructure
         mask_acc = df_acc_['com'] == code
         mask_infra = np.logical_or(df_infra_['code_com_g'] == code, df_infra_['code_com_d'] == code)
         df_infra_city = df_infra_[mask_infra]
         df_acc_city = df_acc_[mask_acc]
         
-        for i_acc in range(df_acc_city.shape[0]):
+        for i_acc in tqdm.tqdm(range(df_acc_city.shape[0])):
             acc = df_acc_city.iloc[i_acc,:]
             acc_index = df_acc_city.iloc[i_acc:i_acc+1,:].index.values[0] #subtefuge pour obtenir l'index
             lat_acc = acc['lat']
             long_acc = acc['long']
             
             #calcul la distance d'un accident à tout les infrastructure d'une même ville
-            dist_series = df_infra_city[['latitude_dep', 'longitude_dep', 'latitude_fin', 'longitude_fin']].apply(distance_infra_acc, axis=1, args=(lat_acc, long_acc))
+            #dist_series = df_infra_city[['latitude_dep', 'longitude_dep', 'latitude_fin', 'longitude_fin']].apply(distance_infra_acc, axis=1, args=(lat_acc, long_acc))
+            dist_series = df_infra_city[['latitude_dep', 'longitude_dep', 'latitude_fin', 'longitude_fin']].apply(distance, axis=1, args =(lat_acc, long_acc))
             
             #Selectionne l'infra la plus proche
             df_acc_.loc[acc_index, "infra_index"] = dist_series.argmin() #reference index of infrastructure
